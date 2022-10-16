d773554c1f28f3207a8f97b56e8494ab07d5774a
Lautaro261 <nahuelygoofy@gmail.com>
2022-10-16 16:26:11 -0300

    falta trabajar en el js

---
+++ ArchivoCosito.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('#for');
+var clickboton = document.querySelector('#boton');
+
+
+form.addEventListener("submit" , function(e){
+    
+    e.preventDefault();
+    console.log(1)
+})
+
+
+
+
+
+
