7435299a0b0232d1adfdc1ea48a33ef35fbb1816
Lautaro261 <nahuelygoofy@gmail.com>
2022-10-29 11:49:56 -0300

    falta estilos en los items

--- ArchivoCosito.js
+++ ArchivoCosito.js
@@ -1,15 +1,40 @@
-const form = document.querySelector('#for');
-var clickboton = document.querySelector('#boton');
+const input = document.querySelector('input')
+const btn = document.querySelector('.btn')
+const ul = document.querySelector('ul');
 
 
-form.addEventListener("submit" , function(e){
-    
+
+
+btn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(1)
-})
+    
+
+    const text = input.value;
+
+    const li= document.createElement('li')
+    const p = document.createElement('p')
+    p.textContent = text;
 
 
+    li.appendChild(p)
+    li.appendChild(btndelete())
+    ul.appendChild(li)
+
+    input.value=""
+
+});
 
 
+function btndelete(){
+    const btndele = document.createElement('button')
+    btndele.textContent = 'X'
+    btndele.className= "btn-delete";
 
+    btndele.addEventListener('click', (e)=>{
+        const item= e.target.parentElement;
+        ul.removeChild(item);
 
+    })
+
+    return btndele
+}
